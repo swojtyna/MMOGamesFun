c8a3fea6ac744ac012406e5d3b970b9633ffdc3a
Sebastian Wojtyna <sebastian.wojtyna@xtb.com>
2022-05-03 17:56:25 +0200

    Init commit

---
+++ MMOGamesFun/MMOGamesFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MMOGamesFun
+//
+//  Created by Sebastian Wojtyna on 03/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
